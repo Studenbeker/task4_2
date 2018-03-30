bae3ca941cf57332a1c4be150e8f220df1e5457f
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-03-31 00:09:13 +0300

    Update ntp_deploy.sh

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Install ntp on the server
+sudo apt-get update -y > /dev/null
 sudo apt-get install -y ntp > /dev/null
 
 #The config files for ntp lies in /etc/ntp.conf.
 
-sed -i "s/0.ubuntu.pool.ntp.org/ua.pool.ntp.org/g" /etc/ntp.conf
-sed -i "/pool 1.ubuntu.pool.ntp.org iburst/d" /etc/ntp.conf
-sed -i "/pool 2.ubuntu.pool.ntp.org iburst/d" /etc/ntp.conf
-sed -i "/pool 3.ubuntu.pool.ntp.org iburst/d" /etc/ntp.conf
+sed -i '/.ubuntu.pool.ntp.org/d' /etc/ntp.conf
+sed -i '/ua.pool.ntp.org/d' /etc/ntp.conf
+sed -i "18i pool ua.pool.ntp.org iburst" /etc/ntp.conf
 
 #Restart the service.
 
