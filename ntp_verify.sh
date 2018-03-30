62959a7241d27641764957505fed9cd29c527add
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-03-30 21:02:55 +0300

    Update ntp_verify.sh

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#ntp_verify.sh
 
 ps auxw | grep ntpd | grep -v grep > /dev/null
 
 if [ $? != 0 ];
 then
-echo "ntp is not running."
-
+echo "NOTICE: ntp is not running"
+echo "NOTICE: ntp is not running" >> /var/mail/root
 service ntp start
 fi
 
 diff /etc/ntp.conf /etc/ntp.conf.back-up > /dev/null
-if [ $? == 1 ]; then
-# they're different!
-echo "ntp.conf was changed."
+if [ $? == 1 ];
+then
+echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:" >> /var/mail/root
+
 diff -u0 /etc/ntp.conf.back-up /etc/ntp.conf
 echo "Restoring ntp.conf and restart ntp server"
 cat /etc/ntp.conf.back-up > /etc/ntp.conf
