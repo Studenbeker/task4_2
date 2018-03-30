de13448d10801943c9e122e4a19efa383569ed22
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-03-30 21:52:01 +0300

    Update ntp_verify.sh

--- ntp_verify.sh
+++ ntp_verify.sh
@@ -14,7 +14,7 @@ if [ $? == 1 ];
 then
 echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:" >> /var/mail/root
 
-diff -u0 /etc/ntp.conf.back-up /etc/ntp.conf
+diff -u0 /etc/ntp.conf.back-up /etc/ntp.conf >> /var/mail/root
 echo "Restoring ntp.conf and restart ntp server"
 cat /etc/ntp.conf.back-up > /etc/ntp.conf
 service ntp restart > /dev/null
