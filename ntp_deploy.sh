dfe154b65f4074060fcef33f5e6977b9d70caf9b
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-04-01 00:41:27 +0300

    Update ntp_deploy.sh

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -3,7 +3,7 @@
 #Install ntp on the server
 sudo apt-get update -y > /dev/null
 sudo apt-get install -y ntp > /dev/null
-
+dirsc=$(dirname $0)
 #The config files for ntp lies in /etc/ntp.conf.
 
 sed -i '/.ubuntu.pool.ntp.org/d' /etc/ntp.conf
@@ -16,5 +16,5 @@ service ntp restart
 
 cp /etc/ntp.conf /etc/ntp.conf.back-up
 
-cron="*/1 * * * * `pwd`/ntp_verify.sh"
+cron="*/1 * * * * $dirsc/ntp_verify.sh"
 (crontab -l 2>/dev/null | grep -v -F "$cron" ; echo "$cron") | crontab -
