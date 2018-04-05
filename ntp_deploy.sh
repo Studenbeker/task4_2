9a1b8756bc0877af41c5cbfd8d7a52464c0d0fc8
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-04-06 01:24:37 +0300

    Update ntp_deploy.sh

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+dirsc=$(dirname $0)
+Installed=$(dpkg -l ntp | grep ii |wc -l)
+if [ $Installed = 0 ]
+then
+
+apt update
 
 #Install ntp on the server
-sudo apt-get update -y > /dev/null
-sudo apt-get install -y ntp > /dev/null
-dirsc=$(dirname $0)
-#The config files for ntp lies in /etc/ntp.conf.
+echo "Installing ntp"
+apt install ntp -y -q
+fi
 
+
+#The config files for ntp lies in /etc/ntp.conf.
 sed -i '/.ubuntu.pool.ntp.org/d' /etc/ntp.conf
 sed -i '/ua.pool.ntp.org/d' /etc/ntp.conf
 sed -i "18i pool ua.pool.ntp.org iburst" /etc/ntp.conf
 
+echo "Creating bak"
+cat /etc/ntp.conf >> /etc/ntp.conf.back-up
+
 #Restart the service.
-
-service ntp restart
-
-cp /etc/ntp.conf /etc/ntp.conf.back-up
+systemctl restart ntp
 
 cron="*/1 * * * * $dirsc/ntp_verify.sh"
 (crontab -l 2>/dev/null | grep -v -F "$cron" ; echo "$cron") | crontab -
